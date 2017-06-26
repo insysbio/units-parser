6d1c5d7645c40f21429806dbe881343c2fccbe13
Victoria Tkachenko <tkachenko@insysbio.ru>
2017-06-26 18:56:31 +0300

    parse unit, hash

--- cases/correct examples.js
+++ cases/correct examples.js
@@ -7,5 +7,9 @@ examples = [ // and hash
   "mg2/L3", // mg2__L3
   "mg/L/s", // mg__L__s
   "mole/kg", // mole__kg
-  "m^1.33/kg^2.2" // m1_33__kg2_2
+  "m^1.33/kg^2.2", // m1_33__kg2_2
+  "mmole2*L/s/Rg2*1/s",
+  "1/L",
+  "L*mole",
+  "mole/1/s"
 ];
--- src/index.js
+++ src/index.js
@@ -9,19 +9,68 @@ class Unit {
   static parseFromString(str) {
     let unit = new Unit();
     
+    console.log("Parse string...");
+    
+    let items = str.match(/([*/]?(1\/)?[A-Za-z]+[\^]?[\d.]*)/g);
+    console.log(" Units and op into str", items);
+    
+    console.log(" Check connect list of units:", simpleUnits);
+    
+    items.forEach(function (item, i) {
+      console.log("elementes of unit ", item[0], item.search(/1\//), item.match(/[A-Za-z]+/), item.match(/[\d.]+$/));
+    
+    unit[i] = {
+      op: item.match(/[$\/*]/) && item.match(/[$\/*]/)[0],
+      kind: item.match(/[A-Za-z]+/)[0],
+      exponent: ((item.search(/1\//) != -1) && (item[0] == "/"))  ? 1 : (
+                ((item.search(/1\//) != -1) || (item[0] == "/")) ? -1 : 1),
+      scale: item.match(/[\d.]+$/) && item.match(/[\d.]+$/)[0]
+    };
+    });
+    console.log("res", unit);
     return unit;
   };
   
   // serialize unit-object to string of type 'mM^2*L/mg/h^2' is the same as 'mM2*L/mg/h2'
   toString(){
-    let res = "";
+    let item, res = "";
     
+    console.log("To string...");
+    for (item in this) {
+      
+    }
     return res;
   }
   
   // serialize unit-object to string of type 'mM2_L__mg__h2'
   toHash(){
-    let res = "";
+    console.log("to Hash");
+    
+    let item, res = "";
+    
+    for (item in this) {
+      console.log(" add hash ", this[item]);
+      
+      if (this[item].op == "*") {
+        res += "_";
+      } else if (this[item].op == "/") {
+        res += "__";
+      }
+      console.log("after add op", res);
+      
+      if ((this[item].op == "/") && (this[item].exponent = 1)) {
+        res += "1__";
+      }
+      console.log("after check 1/s", res);
+      
+      res += this[item].kind;
+      console.log("after add kind", res);
+      
+      if (this[item].scale) {
+        res += this[item].scale;
+      }
+      console.log("after check scale", res);
+    }
     
     return res;
   }
